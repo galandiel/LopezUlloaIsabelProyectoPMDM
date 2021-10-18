aff80e2b31429dbc84d94e498fa9eddedb0c3c09
Isa <isabel.lopez.ulloa@gmail.com>
2021-10-18 10:25:54 +0200

    Creación RegistroActivity

--- app/src/main/java/com/murallaromana/dam/segundo/lopezulloaisabelproyectopmdm/MainActivity.kt
+++ app/src/main/java/com/murallaromana/dam/segundo/lopezulloaisabelproyectopmdm/MainActivity.kt
@@ -2,10 +2,14 @@ package com.murallaromana.dam.segundo.lopezulloaisabelproyectopmdm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.murallaromana.dam.segundo.lopezulloaisabelproyectopmdm.databinding.ActivityMainBinding
+
+private lateinit var binding : ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
